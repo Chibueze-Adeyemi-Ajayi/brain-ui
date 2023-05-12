74f1b51fb69b80f96ebc51bbe4ea58a68f48d36c
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-05-12 09:37:41 +0100

    User Interface

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Loader from './components/loader';
 
 // BRAIN project user interface
 function App() {
   return (
-    <div className="App bg-black flex w-full h-full">
-      <span className="loader text-white w-12 h-12"></span>
+    <div className="App bg-[#0B0E20] flex w-full h-full">
+      {/* <span className="loader text-white w-12 h-12"></span> */}
+      <Loader/>
     </div>
   );
-}
+} 
 
 export default App;
