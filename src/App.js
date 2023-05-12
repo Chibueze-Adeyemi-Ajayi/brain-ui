367a7012d02d8953924871ef61f745e898ea2741
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-05-12 05:35:14 +0100

    User Interface

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-// BRAIN project user interfa
+// BRAIN project user interfac
 function App() {
   return (
     <div className="App bg-black flex w-full h-full">
