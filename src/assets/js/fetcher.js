f9fa1db5274da228e671f9f5b94cb1237231925d
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-05-12 09:58:37 +0100

    User Interface

--- src/assets/js/fetcher.js
+++ src/assets/js/fetcher.js
@@ -1,3 +1,14 @@
-export function fetcher () {
+import { sendRequest } from "./http";
 
+export function fetcher () {
+    console.log("processing ....");
+    const url = 'brain/query/';
+    const requests = {
+        query: "prompt engineering",
+        role: "assistant"
+    }
+    sendRequest(url, requests, res => {
+        console.log(res);
+        console.log("Thank you");
+    });
 }
