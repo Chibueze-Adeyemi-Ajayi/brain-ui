ea562d292375fe054e091cc444f369b4d9f1a6f3
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-05-13 07:01:43 +0100

    BRAIN UI

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { FaSearch } from "react-icons/fa";
 
 function NavigationBar() {
   return (
@@ -18,7 +17,9 @@ function NavigationBar() {
             placeholder="Search"
             className="border border-gray-300 rounded-md py-2 px-4 pr-10 focus:outline-none focus:ring-1 focus:ring-blue-500"
           />
-          <FaSearch className="absolute top-2 right-3 text-gray-400" />
+          <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path className="fill-gray-500" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/>
+          </svg>
         </div>
         <button className="ml-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
           Search
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,4 +1,4 @@
-import NavigationBar from "../components/footer";
+import NavigationBar from "../components/nav";
 
 export default function Search () {
     return <section className="w-full h-full flex flex-col">
