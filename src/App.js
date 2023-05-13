aa51f314eb36380e19557e9251ecf437f1d6af8f
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-05-13 05:23:13 +0100

    BRAIN UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Loader from './components/loader';
 import { fetcher } from './assets/js/fetcher';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/home';
 
 // BRAIN project user interface
 function App() {
   // consuming the brain api
   useEffect(fetcher, []);
+  const [page_var, page_func] = useState(false);
   return (
     <div className="App bg-[#0B0E20] flex w-full h-full">
       {/* <span className="loader text-white w-12 h-12"></span> */}
-      <Loader/>
+      {!page_var ? <Loader/> : <Router>
+        <Switch>
+          <Route exact path="/" component={<Home/>} />
+          <Route path="/home" component={<Home/>} />
+        </Switch>
+      </Router>}
     </div>
   );
 } 
---
+++ src/pages/home.js
@@ -0,0 +1,3 @@
+export default function Home (props) {
+    return <section>Jilo Billionaire</section>
+}
