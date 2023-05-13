ab758cc34266f71bd97c4ae0ad7070b427841640
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-05-13 15:15:43 +0100

    BRAIN UI

---
+++ src/components/picture-card.js
@@ -0,0 +1,25 @@
+function PictureCard() {
+    return (
+      <div className="max-w-xs rounded-lg overflow-hidden shadow-lg">
+        <img
+          className="w-full h-56 object-cover"
+          src="https://placeimg.com/640/480/nature"
+          alt="Nature"
+        />
+        <div className="p-4">
+          <h2 className="text-xl font-bold text-gray-800 mb-2">Picture Card</h2>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac
+            eleifend nisl. Mauris fringilla dapibus semper.
+          </p>
+          <div className="mt-4">
+            {/* <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
+              Read More
+            </button> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  export default PictureCard;
--- src/pages/all.js
+++ src/pages/all.js
@@ -1,10 +1,21 @@
+import PictureCard from "../components/picture-card";
 import TextCard from "../components/text-card";
 
 export default function AllPage () {
-    return <section className="w-full p-3 md:p-6 mt-[55px] h-full space-y-3">
+    return <section className="w-full p-3 md:p-6 mt-[55px] h-full space-y-6">
         <section className="w-full flex">
             <div className="w-[65%] mx-auto">
                 <TextCard/>
+            </div>
+        </section>
+        <section className="w-full flex">
+            <div className="w-[65%] mx-auto space-y-6">
+                <h3 className="font-bold text-2xl">Image Results</h3>
+                <div className="w-full h-fit grid grid-cols-3 gap-3">
+                    <PictureCard/> <PictureCard/>
+                    <PictureCard/> <PictureCard/>
+                    <PictureCard/> <PictureCard/>
+                </div>
             </div>
         </section>
     </section>
