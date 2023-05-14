e98089a5ac3ab3e198455bb9c76a5dd62a898ec5
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-05-14 12:37:30 +0100

    BRAIN UI

--- src/pages/all.js
+++ src/pages/all.js
@@ -13,11 +13,19 @@ export default function AllPage () {
 
     const [loading_var, loading_func] = useState(true);
     const [err_var, err_function] = useState(false);
+    const [page_data_var, page_data_func] = useState({
+        general_informations:"", suggested_links: [],
+        image_results: [], video_results: [],
+        news_results: [], map_results: [],
+        suggested_searches: []
+    })
 
+    // searching for default message
     useEffect(() => {
         searchForAllResults((status, results) => {
             err_function(status ? false : true); // checking for error message 
             if (!status) return;
+            loading_func(false);
             console.log(results);
         });
     }, []);
@@ -28,64 +36,42 @@ export default function AllPage () {
 
         <section className="w-full flex flex-col">
             <div className="w-[65%] mx-auto border-b border-gray-200 pb-8">
-                <TextCard/> <br></br><br></br>
+                {page_data_var.general_informations} <br></br><br></br>
                 <h3 className="font-bold text-xl text-gray-800">Suggested Links</h3> <br></br>
-                <div className="w-full h-fit grid grid-cols-3 gap-3">
-                    <LinkCard/> <LinkCard/>
-                    <LinkCard/> <LinkCard/>
-                    <LinkCard/> <LinkCard/>
-                </div>
+                <div className="w-full h-fit grid grid-cols-3 gap-3">{page_data_var.suggested_links}</div>
             </div>
         </section>
 
         <section className="w-full flex">
             <div className="w-[65%] mx-auto space-y-6 pb-16 border-b border-gray-200">
                 <h3 className="font-bold text-xl text-gray-800">Image Results</h3>
-                <div className="w-full h-fit grid grid-cols-3 gap-3">
-                    <PictureCard/> <PictureCard/>
-                    <PictureCard/> <PictureCard/>
-                    <PictureCard/> <PictureCard/>
-                </div>
+                <div className="w-full h-fit grid grid-cols-3 gap-3">{page_data_var.image_results}</div>
             </div>
         </section>
 
         <section className="w-full flex">
             <div className="w-[65%] mx-auto space-y-6 pb-16 border-b border-gray-200">
                 <h3 className="font-bold text-xl text-gray-800">Video Results</h3>
-                <div className="w-full h-fit grid grid-cols-3 gap-3">
-                    <VideoCard/> <VideoCard/>
-                    <VideoCard/> <VideoCard/>
-                    <VideoCard/> <VideoCard/>
-                </div>
+                <div className="w-full h-fit grid grid-cols-3 gap-3">{page_data_var.video_results}</div>
             </div>
         </section>
         <section className="w-full flex">
             <div className="w-[65%] mx-auto space-y-6 pb-16 border-b border-gray-200">
                 <h3 className="font-bold text-xl text-gray-800">News</h3>
-                <div className="w-full h-fit grid grid-cols-3 gap-3">
-                    <NewsCard/> <NewsCard/>
-                    <NewsCard/> <NewsCard/>
-                    <NewsCard/> <NewsCard/>
-                </div>
+                <div className="w-full h-fit grid grid-cols-3 gap-3">{page_data_var.news_results}</div>
             </div>
         </section>
         <section className="w-full flex">
             <div className="w-[65%] mx-auto space-y-6 pb-16 border-b border-gray-200">
                 <h3 className="font-bold text-xl text-gray-800">Map</h3>
-                <div className="w-full h-fit grid grid-cols-3 gap-3">
-                    <GoogleMap />
-                </div>
+                <div className="w-full h-fit grid grid-cols-3 gap-3">{page_data_var.map_results}</div>
             </div>
         </section>
         
         <section className="w-full flex">
             <div className="w-[65%] mx-auto space-y-6">
                 <h3 className="font-bold text-xl text-gray-800">Suggested Search</h3>
-                <div className="w-full h-fit grid grid-cols-3 gap-3">
-                    <LinkCard/> <LinkCard/>
-                    <LinkCard/> <LinkCard/>
-                    <LinkCard/> <LinkCard/>
-                </div>
+                <div className="w-full h-fit grid grid-cols-3 gap-3">{page_data_var.suggested_searches}</div>
             </div>
         </section>
 
