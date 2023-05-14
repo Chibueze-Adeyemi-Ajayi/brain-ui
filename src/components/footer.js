9f82632ea4af21f4a4292d33628fead3438b7cb4
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-05-14 12:03:15 +0100

    BRAIN UI

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+      <footer className="bg-gray-100 py-4">
+        <div className="container mx-auto text-center text-gray-500">
+          <p className="text-sm">
+            &copy; {new Date().getFullYear()} Jilo Innovations. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    );
+  }
+
+  export default Footer;
